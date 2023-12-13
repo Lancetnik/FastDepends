ce5034ec5d4099bb0ba7fc7620fc97c9f35135e2
Nikita Pastukhov <diementros@yandex.ru>
2023-12-13 22:15:22 +0300

    feat (#34): inject to classes

---
+++ tests/async/test_class.py
@@ -0,0 +1,22 @@
+import pytest
+
+from fast_depends import Depends, inject
+
+
+def _get_var():
+    return 1
+
+
+class Class:
+    @inject
+    def __init__(self, a = Depends(_get_var)) -> None:
+        self.a = a
+
+    @inject
+    async def calc(self, a = Depends(_get_var)) -> int:
+        return a + self.a
+
+
+@pytest.mark.anyio
+async def test_class():
+    assert await Class().calc() == 2
---
+++ tests/sync/test_class.py
@@ -0,0 +1,19 @@
+from fast_depends import Depends, inject
+
+
+def _get_var():
+    return 1
+
+
+class Class:
+    @inject
+    def __init__(self, a = Depends(_get_var)) -> None:
+        self.a = a
+
+    @inject
+    def calc(self, a = Depends(_get_var)) -> int:
+        return a + self.a
+
+
+def test_class():
+    assert Class().calc() == 2

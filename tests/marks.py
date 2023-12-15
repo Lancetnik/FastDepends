928e945007eb3bebf55593ab774f40201e96342f
Nikita Pastukhov <diementros@yandex.ru>
2023-12-15 18:36:25 +0300

    chore: rm useless code

--- tests/marks.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from fast_depends._compat import PYDANTIC_V2
-
-pydanticV1 = pytest.mark.skipif(PYDANTIC_V2, reason="requires PydanticV2")
-
-pydanticV2 = pytest.mark.skipif(not PYDANTIC_V2, reason="requires PydanticV1")

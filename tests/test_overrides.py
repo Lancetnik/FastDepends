1fb1f135901aad692c456f451e0e566aa4a68afc
Nikita Pastukhov <diementros@yandex.ru>
2025-08-12 16:43:22 +0300

    tests: validate #163 problem

--- tests/test_overrides.py
+++ tests/test_overrides.py
@@ -211,3 +211,67 @@ def test_deep_overrides_with_different_signatures(provider: Provider) -> None:
         assert not mock.dep1.called
         assert not mock.dep2.called
         mock.dep3.assert_called_once()
+
+
+def test_override_context_with_generator(provider: Provider) -> None:
+    def base_dep() -> Generator[int, None, None]:
+        yield 1
+
+    def override_dep() -> Generator[int, None, None]:
+        yield 2
+
+    @inject(dependency_provider=provider)
+    def func(d=Depends(base_dep)):
+        return d
+
+    with provider.scope(base_dep, override_dep):
+        assert func() == 2
+
+
+def test_override_context_with_undefined_generator(provider: Provider) -> None:
+    def base_dep() -> Generator[int, None, None]:
+        raise NotImplementedError
+
+    def override_dep() -> Generator[int, None, None]:
+        yield 2
+
+    @inject(dependency_provider=provider)
+    def func(d=Depends(base_dep)):
+        return d
+
+    with provider.scope(base_dep, override_dep):
+        assert func() == 2
+
+
+@pytest.mark.anyio
+async def test_async_override_context_with_generator(provider: Provider) -> None:
+    async def base_dep() -> Generator[int, None, None]:
+        yield 1
+
+    async def override_dep() -> Generator[int, None, None]:
+        yield 2
+
+    @inject(dependency_provider=provider)
+    async def func(d=Depends(base_dep)):
+        return d
+
+    with provider.scope(base_dep, override_dep):
+        assert await func() == 2
+
+
+@pytest.mark.anyio
+async def test_async_override_context_with_undefined_generator(
+    provider: Provider,
+) -> None:
+    async def base_dep() -> Generator[int, None, None]:
+        raise NotImplementedError
+
+    async def override_dep() -> Generator[int, None, None]:
+        yield 2
+
+    @inject(dependency_provider=provider)
+    async def func(d=Depends(base_dep)):
+        return d
+
+    with provider.scope(base_dep, override_dep):
+        assert await func() == 2

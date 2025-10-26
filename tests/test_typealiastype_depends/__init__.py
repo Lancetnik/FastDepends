3fe24a32bde4df605765f794296f5729601b670f
lucasfrancaid <lucasfrancaid@gmail.com>
2025-10-26 15:47:24 +0000

    fix: unwrap original key when its wrapped to not duplicate dependency

--- tests/test_typealiastype_depends/__init__.py
+++ tests/test_typealiastype_depends/__init__.py
@@ -1,5 +1,6 @@
 import sys
+
 import pytest
 
 if sys.version_info < (3, 12):
-    pytest.skip("Test for Python >= 3.12",allow_module_level=True)
+    pytest.skip("Test for Python >= 3.12", allow_module_level=True)
